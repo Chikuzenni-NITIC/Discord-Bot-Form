587d752af75b07d9a2f9d3fcce1905e556d61aec
sora <k.magro29@gmail.com>
2022-10-29 00:02:37 +0900

    Fix

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,21 +1,21 @@
+// 選択された画像をプレビューする
 document.getElementById('img').addEventListener('change', function (e) {
-    // 1枚だけ表示する
     const file = e.target.files[0];
-
-    // ファイルのブラウザ上でのURLを取得する
     const blobUrl = window.URL.createObjectURL(file);
-
-    // img要素に表示
 	const imgForm = document.querySelector(".img_label_style")
-	imgForm.classList.toggle("disable")
 	const imgView = document.querySelector(".img_selected_img")
 	const imgViewCont = document.querySelector(".img_selected")
 	imgView.src = blobUrl
+	imgForm.classList.toggle("disable")
 	imgViewCont.classList.toggle("disable")
 })
+
+// 画像の選択を取り消す
 document.querySelector(".cancel").addEventListener('click', function (e) {
 	const imgForm = document.querySelector(".img_label_style")
+	const imgView = document.querySelector(".img_selected_img")
 	const imgViewCont = document.querySelector(".img_selected")
 	imgForm.classList.toggle("disable")
 	imgViewCont.classList.toggle("disable")
+	imgView.src = ""
 })
