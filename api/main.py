b8ef4e78f3be9b86440a4108d38c6d239bfdf4c0
Gon-1222 <caramelpopsoft@gmail.com>
2022-10-29 07:52:08 +0900

    Add reqests

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request,abort
 import os
 import requests
-#url_items=os.environ["WEBHOOK_URL"]
+url_items=os.environ["WEBHOOK_URL"]
 headers={"Content-Type": "application/json"}
 
 app = Flask(__name__,static_folder='static')
@@ -16,7 +16,7 @@ def index():
         "username": "匿名さん",
         "avatar_url": "https://thumb.ac-illust.com/89/89e6bfc83866f18e36bd905fe9a07ea2_t.jpeg",
         "content": text,}
-        #requests.post(url_items, headers=headers, json=item_data)
+        requests.post(url_items, headers=headers, json=item_data)
         return render_template('success.html') #5 受付完了画面
     return render_template('index.html') #6 GETの処理
 
