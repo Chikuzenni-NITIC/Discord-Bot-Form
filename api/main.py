1cf5e8c9522da14eae9fd0cb7f1cbcad2ad1f5a1
sora <k.sora151201@gmail.com>
2023-01-08 14:12:12 +0900

    feat #2 uploading file
    画像のアップロードをサーバー側の処理に変更

--- api/main.py
+++ api/main.py
@@ -1,24 +1,42 @@
 from flask import Flask, render_template, request,abort
 import os
 import requests
+import json
 url_items=os.environ["WEBHOOK_URL"]
-headers={"Content-Type": "application/json"}
 
 app = Flask(__name__,static_folder='static')
 
 @app.route('/', methods = ["GET" , "POST"]) #2 GETとPOSTをリクエストできるようにする
 def index():
     if request.method == 'POST': #3POSTの処理
-        print(request.form) #4 formのname属性を取得
-        print(request.form['image']) #4 formのname属性を取得
-        text='\n'.join([request.form["message"],request.form['image']])
+        text = request.form["message"] #4 formのname属性を取得
+        file_form = request.files["file"] 
+        filename = file_form.filename
+        
         item_data = {
-        "username": "匿名さん",
-        "avatar_url": "https://thumb.ac-illust.com/89/89e6bfc83866f18e36bd905fe9a07ea2_t.jpeg",
-        "content": text,}
-        requests.post(url_items, headers=headers, json=item_data)
+            "username": "匿名さん",
+            "avatar_url": "https://thumb.ac-illust.com/89/89e6bfc83866f18e36bd905fe9a07ea2_t.jpeg",
+            "content": text,
+        }
+
+        if filename:
+            data = {
+                "payload_json": json.dumps(item_data)
+            }
+            files = {
+                "attachment": (filename, file_form.read())
+            }
+            headers = None
+        else:
+            data = json.dumps(item_data)
+            files = None
+            headers={
+                "Content-Type": "application/json"
+            }
+
+        requests.post(url_items, data, headers=headers, files=files)
         return render_template('success.html') #5 受付完了画面
     return render_template('index.html') #6 GETの処理
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80, debug=True)
+    app.run(host = '0.0.0.0', port = 80)
